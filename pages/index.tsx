f86a694d67fc14387de604c6acbc138a98aa8634
Jean Pablo <jeanpablocalderonramirez@gmail.com>
2023-04-20 01:14:27 +0200

    changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,14 +24,14 @@ export default function Home() {
               <li>
                 <Link  
                   href={''} 
-                  className="text-sm duration-500 text-zinc-500 hover:text-zinc-300">
+                  className="cursor-none text-sm duration-500 text-zinc-500 hover:text-zinc-300">
                    Projects
                 </Link>
               </li>
               <li>
                 <Link  
                   href={''} 
-                  className="text-sm duration-500 text-zinc-500 hover:text-zinc-300">
+                  className="cursor-none text-sm duration-500 text-zinc-500 hover:text-zinc-300">
                    Contact
                 </Link>
               </li>
@@ -40,7 +40,7 @@ export default function Home() {
           {/* Content */}
           <div className='hidden w-screen h-px md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0'></div>
           {/* Hover Card */}
-          <h1 className='z-10 text-4xl tracking-wide font-nunito duration-1000 cursor-default animate-title  font-display sm:text-6xl md:text-9xl whitespace-nowrap text-transparent bg-gradient-to-t bg-clip-text from-zinc-100/50 to-white '>
+          <h1 className='z-10 text-4xl tracking-wide font-nunito duration-1000 animate-title  font-display sm:text-6xl md:text-9xl whitespace-nowrap text-transparent bg-gradient-to-t bg-clip-text from-zinc-100/50 to-white '>
             Δrkift
           </h1>
           {/* End Hover Card */}
