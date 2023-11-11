51171d9b3f00a2f28134b9a6d41e7c5f0f836e6e
Jean Pablo <jeanpablocalderonramirez@gmail.com>
2023-11-11 02:10:48 +0100

    changes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 export const Footer = () => {
   return (
     <footer className="py-28">
-      <div className="flex flex-row gap-5 [&_a]:text-blue-100/40">
+      <div className="flex flex-row items-center gap-5 [&_a]:text-blue-100/40">
         <Link href="https://www.linkedin.com/in/jean-ram/" target="_blank" rel="nofollow noreferrer noopener">Linkedin</Link>
         <Link href="https://twitter.com/arkift_r" target="_blank" rel="nofollow noreferrer noopener">Twitter</Link>
         <Link href="https://github.com/raburuz" target="_blank" rel="nofollow noreferrer noopener">Github</Link>
